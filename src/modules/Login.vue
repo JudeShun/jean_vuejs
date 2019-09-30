269d96ac7e0091b7af4507b0722d025d73fce1c0
Gracejean <jennilyn.mendoza@student.passerellesnumeriques.org>
2019-09-30 16:14:39 +0800

    Initial commit

---
+++ src/modules/Login.vue
@@ -0,0 +1,114 @@
+<template>
+
+  <div class="row loginPage">
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+        <div class="container">
+          <div class="card">
+            <div class="card-header">LOGIN</div>
+            <div class="card-body">
+              <div class="form-group">
+                <label for="email" class="bmd-label-floating">Email</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="email"
+                  v-model="email"
+                  placeholder="Enter email..."
+                />
+              </div>
+              <div class="form-group">
+                <label for="pwd" class="bmd-label-floating">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="passw"
+                  v-model="password"
+                  placeholder="Enter password..."
+                />
+              </div>
+              <center>
+                <button type="button" class="btn btn-outline-success login-btn" id="btnLogin" @click='submit'>Login</button>
+                
+              </center>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </template>
+<style scoped lang="scss">
+@import "assets/colors.scss";
+
+#username {
+  color: $primary !important;
+}
+#pass {
+  color: $primary !important;
+}
+</style>
+<script>
+import AUTH from 'services/auth'
+export default {
+  data() {
+    AUTH
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    submit: function(e) {
+      e.preventDefault();
+      AUTH.login(this.email, this.password)
+    },
+
+  }
+};
+</script>
+
+<style scoped>
+  .card {
+  background-color: pink;
+  margin-top: 50px;
+  padding: 30px;
+}
+  
+
+.h3 {
+  margin-left: 500px;
+  margin-top: 300px;
+  font-size: 40px;
+  font-family: Times new;
+}
+.nav {
+  margin-block: auto;
+}
+
+.card-header {
+  text-align: center;
+  font-weight:bold;
+  font-size: 30px;
+  letter-spacing: 5px;
+  background-color: black;
+  color:pink;
+  
+}
+
+label {
+  font-weight:bold;
+}
+
+input {
+  border: 2px solid black;
+}
+
+#btnLogin {
+  color: black;
+  border: 2px solid black;
+  margin-top: 5px;
+  padding: 12px 30px;
+}
+
+</style>
---
+++ src/router/frame/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" class="bg-dark" variant="info">
+      <b-navbar-brand href="#" class="text text-light" id="title">JM APP</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-navbar-nav>
+            <b-nav-item @click="redirect('/Login')" class="text text-light" id="login">Login</b-nav-item>
+            <b-nav-item @click="redirect('/Register')" class="text text-light" id="signup">Sign Up</b-nav-item>
+          </b-navbar-nav>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+<style scoped lang="scss">
+@import "assets/colors.scss";
+#title {
+  color: $danger !important;
+}
+</style>
+<script>
+import ROUTER from "router";
+export default {
+  methods: {
+    redirect(router) {
+      ROUTER.push(router);
+    }
+  }
+};
+</script>
